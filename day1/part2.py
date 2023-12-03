6a5b04e714724303cb7fdddf0a54315255b34a10
Rigel Shen <rigelshen@gmail.com>
2023-12-02 18:55:01 -0800

    Adding my solution for Day 1 Part 2

--- day1/part2.py
+++ day1/part2.py
@@ -3,21 +3,25 @@ listOfLines = []
 with open('input.txt') as file:
     lines = file.readlines()
     for line in lines:
-        numericWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-        numerics = {
-            'one': '1',
-            'two': '2',
-            'three': '3',
-            'four': '4',
-            'five': '5',
-            'six': '6',
-            'seven': '7',
-            'eight': '8',
-            'nine': '9'
-        }
-        for i in range(len(numericWords)):
-            line = line.replace(i[num], )
-        line.replace()
+        numericWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+        word = ""
+        for char in line:
+            if char.isdigit():
+                break
+            word = word + char
+            for i in range(len(numericWords)):
+                if numericWords[i] in word:
+                    line = line.replace(numericWords[i], str(i), 1)
+                    break
+        word = ""
+        for char in reversed(line):
+            if char.isdigit():
+                break
+            word = char + word
+            for i in range(len(numericWords)):
+                if numericWords[i] in word:
+                    line = line.replace(numericWords[i], str(i))
+                    break
         numsOnly = list(filter(lambda i: i.isdigit(), line))
         number = numsOnly[0] + numsOnly[len(numsOnly) -1 ]
         listOfLines.append(int(number))
