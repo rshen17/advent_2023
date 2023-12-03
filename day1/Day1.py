710a15f780a6d3ad43e4d02d4f28d2f5538bec5e
pniko50 <pniko50@yahoo.com>
2023-12-03 16:08:32 -0700

    Day 1 advent of test Python Finished

--- day1/Day1.py
+++ day1/Day1.py
@@ -33,25 +33,37 @@ class Day1:
         if self.part == 'A':
             pattern = re.compile(r'\d+')
         else:
-            pattern = re.compile(r'(\d|one|two|three|four|five|sixs|eveneightnine)+')
+
+            pattern = re.compile(r'(\d|one|two|three|four|five|six|seven|eight|nine)')
+            reverse = r')d\|one|two|three|four|five|six|seven|eight|nine('[::-1]
+            pattern2 = re.compile(reverse)
 
         value_list = []
 
         for string in input_array:
-            print(string)
+            print(string.strip())
             occurences = re.findall(pattern, string)
+            if self.part == 'B':
+                reverse = re.findall(pattern2, string[::-1])
+
+            occurences = [m for m in occurences if m]
             print(occurences)
             first = occurences[0]
-            last = occurences[-1]
-            print(first)
-            print(last)
+            if self.part == 'B':
+                last = reverse[0][::-1]
+            else:
+                last = occurences[-1]
+
+            # print(first)
+            # print(last)
             if first in self.regexdict:
                 first = self.regexdict[first]
             first = first[0]
             if last in self.regexdict:
                 last = self.regexdict[last]
             last = last[-1]
-            calibration_value = int(first+last)
+            calibration_value = int(first + last)
+            print(calibration_value)
             value_list.append(calibration_value)
 
         totalsum = sum(value_list)
@@ -72,7 +84,7 @@ if __name__ == '__main__':
     if args.part.upper() in ['A', 'B']:
         data_file_name = "{1}{0}.txt".format(args.part.upper(), args.data)
         if os.path.exists(data_file_name):
-            test = Day1(data_file_name,args.part.upper())
+            test = Day1(data_file_name, args.part.upper())
             test.run()
         else:
             raise FileNotFoundError()
